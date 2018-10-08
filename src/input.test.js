93228d35c8a4c79a49f51dc499f4bf679b913acf
sibyldawn <sibyldawn@gmail.com>
2018-10-08 15:05:44 -0700

    Add Input and Submit Button

--- src/input.test.js
+++ src/input.test.js
@@ -6,23 +6,30 @@ import Input from './input';
 
 const setup = (initialState = {}) => {
     const store = storeFactory(initialState)
-const wrapper = shallow(<Input store = {store}/>);
-    console.log(wrapper.debug());
+    const wrapper = shallow(<Input store = {store}/>).dive();
+    return wrapper;
 }
 
-setup()
 describe('render',() => {
     describe('word has not been guessed', () => {
+        let wrapper;
+        beforeEach(() => {
+            const initialState = { success: false };
+            wrapper = setup(initialState);
+        })
         test('renders component without error', () => {
-
+            const component = findByTestAttr(wrapper, 'component-input');
+            expect(component.length).toBe(1)
         });
 
         test('renders input box',() => {
-
+            const inputBox = findByTestAttr(wrapper, 'input-box');
+            expect(inputBox.length).toBe(1)
         });
         
         test('renders submit button', () => {
-
+            const submitButton = findByTestAttr(wrapper, 'submit-button');
+            expect(submitButton.length).toBe(1)
         });
     });
 
