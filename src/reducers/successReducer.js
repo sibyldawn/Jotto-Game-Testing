1a05c0ac07493efd549a4807d25d33ce855ddce5
sibyldawn <sibyldawn@gmail.com>
2018-10-08 14:14:25 -0700

    Write code and test for Success Reducer

--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -1,5 +1,10 @@
+import { actionTypes } from '../actions';
 
-
-export default (state,action) => {
-    return null;
+export default (state=false,action) => {
+    switch(action.type){
+        case(actionTypes.CORRECT_GUESS):
+         return true;
+         default:
+         return state;
+    }
 }
