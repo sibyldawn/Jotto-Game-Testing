d735a89a649210d5b94a01f97f836474e0a8a63d
sibyldawn <sibyldawn@gmail.com>
2018-10-08 10:39:05 -0700

    Complete Congrats Component

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const Congrats = (props) => {
-    return (
-        <div>Congrats</div>
-    );
-};
-
-export default Congrats;
+export default (props) => {
+        if(props.success){
+            return(
+            <div data-test="component-congrats">
+                <span data-test="congrats-message">
+                Congratulations! You guessed the word!
+                </span>
+            </div>
+            )
+        }else{
+            return (
+                <div data-test="component-congrats"/>
+            )
+        }
+}
