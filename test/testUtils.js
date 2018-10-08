f8270072191d4363313d63e95fae386e143a587e
sibyldawn <sibyldawn@gmail.com>
2018-10-08 14:40:46 -0700

    Add Input Code and Test

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,5 +1,18 @@
 import checkPropTypes from 'check-prop-types';
+import { createStore } from 'redux';
 
+import rootReducer from '../src/reducers';
+
+/**
+ * Create a testing store with imported reducers, middleware, and initial state
+ * globals: rootReducer
+ * @param {object} initialState - Initial state for store.
+ * @function storeFactory
+ * @returns {Store} -Redux Store 
+ */
+export const storeFactory = (initialState) => {
+    return createStore(rootReducer,initialState);
+}
 
 export const findByTestAttr = (wrapper, val)=>{
   return wrapper.find(`[data-test="${val}"]`);
